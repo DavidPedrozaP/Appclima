0579009c19423521e0e5d535665a6a1528de5c11
david pedroza <davy.pedroza.p@gmail.com>
2023-01-29 17:01:16 -0500

    version 1

---
+++ src/components/Loader.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+const Loader = () => {
+  return (
+    <div className="lds-spinner">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Loader;
